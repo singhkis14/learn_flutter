8964cf1002614e6abc22399e5a42b7ae00eb1039
singhkis <k4utech@gmail.com>
2024-11-19 22:43:16 +0530

    Provider manual listner on state change

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,11 +5,26 @@ import 'package:learn_flutter/provider/activity_provider.dart';
 
 // https://riverpod.dev/docs/essentials/first_request
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.listenManual(activityProvider, (previous, next) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('State changed')));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final AsyncValue<Activity> activity = ref.watch(activityProvider);
 
     return Center(
